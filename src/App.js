7ff7d01707995bce23f17309afbad92d720408dd
MauroMoyano <mauromoyanodev@gmail.com>
2023-06-19 15:20:30 -0300

    socials ok

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 // import components
 import Header from './components/Header';
+import Hero from "./components/Hero";
 
 const App = () => {
   return <div>
     <Header/>
+    <Hero />
     <div style={{height: '2000px'}}></div>
   </div>;
 };
