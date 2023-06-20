b4a790e40161be1a599bb308819f169bdb6a5e2f
MauroMoyano <mauromoyanodev@gmail.com>
2023-06-20 06:25:39 -0300

    Banner ok

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 // import components
 import Header from './components/Header';
 import Hero from "./components/Hero";
+import Brands from "./components/Brands";
 
 const App = () => {
   return <div>
     <Header/>
     <Hero />
+    <Brands />
     <div style={{height: '2000px'}}></div>
   </div>;
 };
