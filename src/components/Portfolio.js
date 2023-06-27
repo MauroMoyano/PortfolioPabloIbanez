5a527b9797c55bade6d1079e6dcf5e5e6164f0bd
MauroMoyano <mauromoyanodev@gmail.com>
2023-06-26 23:19:42 -0300

    starting section Portfolio

---
+++ src/components/Portfolio.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Import components
+import Project from "./Project";
+
+const Portfolio = () => {
+    return <section id='portfolio' className='section'>Portfolio</section>;
+};
+
+export default Portfolio;
